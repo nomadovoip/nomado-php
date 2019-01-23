0d3b23930f1cdc7038d31bdb3c90fa312e77459d
Flavien <flavien@bephoto.be>
2019-01-23 17:40:01 +0100

    add otp

---
+++ src/Api/Otp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Nomado\Api;
+
+use Nomado\Common\Response;
+
+class Otp extends Api
+{
+
+    /**
+     * @param $options
+     * @return Response
+     */
+    public function create($options) {
+        $endpoint = '2fa/create';
+        $requiredParams = ['to'];
+
+        return $this->_call($endpoint, 'post', $options, $requiredParams);
+    }
+
+    /**
+     * @param $options
+     * @return Response
+     */
+    public function verify($options) {
+        $endpoint = '2fa/verify';
+        $requiredParams = ['number', 'token'];
+
+        return $this->_call($endpoint, 'post', $options, $requiredParams);
+    }
+
+}
--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,7 @@
 
 namespace Nomado;
 
+use Nomado\Api\Otp;
 use Nomado\Api\Sms;
 use Nomado\Common\Authentication;
 use Nomado\Common\Nomado;
@@ -24,5 +25,6 @@ class Client
         $nomadoClient = new Nomado($authentication);
 
         $this->sms = new Sms($nomadoClient);
+        $this->otp = new Otp($nomadoClient);
     }
 }
